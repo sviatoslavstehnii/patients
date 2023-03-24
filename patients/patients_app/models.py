84fd7caf6b2c2139c0e6dcbde61648505bb48846
taraslysun <taraslysun2@gmail.com>
2023-03-24 12:42:14 +0200

    calendar

--- patients/patients_app/models.py
+++ patients/patients_app/models.py
@@ -20,3 +20,9 @@ class Patient(models.Model):
     class META:
         """Meta class for Patient model."""
         ordering = ['name']
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
